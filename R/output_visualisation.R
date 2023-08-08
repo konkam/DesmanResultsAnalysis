dad4774ffd4d8061122894bcc33d3726a9fc0e49
konkam <guillaume.konkamking.work@gmail.com>
2023-08-08 17:35:24 +0200

    filter seems to have problems inside the package

--- R/output_visualisation.R
+++ R/output_visualisation.R
@@ -1,7 +1,10 @@
 compute_relative_abundance <- function(number_of_variants = 1, chain = 5, include_warmup = FALSE, prefix = "", quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975)) {
   load_gamma_parameter(number_of_variants = number_of_variants, prefix = prefix, include_warmup = include_warmup, nchains = chain) %>%
     ggmcmc::ggs() %>%
-    filter(Chain == chain) %>%
+    # dplyr::filter(Chain == chain) %>%
+    (function(df){
+      df[df$Chain == chain,]
+    }) %>%
     calculate_quantiles_by_factor(factor_column = "Parameter", value_column = "value", quantiles = quantiles, add_mean = T) %>%
     dplyr::mutate(
       Variant = Parameter %>% stringr::str_split_i("_", i = 2),
@@ -17,7 +20,7 @@ compute_relative_abundance <- function(number_of_variants = 1, chain = 5, includ
 #' @param chain The chain selected. This ought to be the chain with the lowest deviance among all chains.
 #' @param prefix File path describing where the results are stored.
 #' @importFrom dplyr mutate
-#' @importFrom ggplot2 geom_segment scale_x_continuous
+#' @importFrom ggplot2 geom_segment scale_x_continuous geom_point
 #'
 #' @return A plot the relative abundance for each variants in each sample
 #' @export
