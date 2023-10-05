0223f6ce945936adc6c5f79d6a3bf799aa3ad642
konkam <guillaume.konkamking.work@gmail.com>
2023-10-05 18:58:58 +0200

    some functions to collect variants

---
+++ R/collect_all_variants.R
@@ -0,0 +1,3 @@
+collect_variants_one_chain = function(tau_sample_one_chain){
+  n_variants = dim(tau_sample_one_chain)[2]
+}
--- R/helper_functions.R
+++ R/helper_functions.R
@@ -53,3 +53,87 @@ calculate_quantiles_by_factor <- function(input_table, factor_column, value_colu
 
   return(output_table)
 }
+
+nucleotides_letters <- c("A", "C", "G", "T")
+nucleotides_indicator_vector = lapply(1:4, function(i){diag(x = 1, nrow = 4)[i,]})
+nucleotides_dict = c(nucleotides_letters, nucleotides_indicator_vector) %>% (function(v) v %>% setNames(rev(v)))
+nucleotides_conversion_function = function(nms){
+  sapply(nms, function(nm){nucleotides_dict[nm]})
+}
+
+
+#' Title
+#'
+#' @param input_tibble
+#' @param columns_to_check
+#'
+#' @return
+#'
+#' @examples
+#' # Sample data
+#' df <- tibble(
+#'   ID = c(1, 2, 3, 4, 5, 6),
+#'   A = c(10, 20, 10, 30, 5, 6),
+#'   B = c(5, 5, 5, 5, 5, 5),
+#'   C = c("X", "Y", "X", "Z", "Z", "Z")
+#' )
+#'
+#' #Only check columns A and B
+#' result_df1 <- filter_rows_not_all_equal(df, columns_to_check = c("A", "B"))
+#' print(result_df1)
+#'
+#' # Check all columns except ID
+#' result_df2 <- filter_rows_not_all_equal(df, columns_to_exclude = c("C", "ID"))
+#' print(result_df2)
+#'
+#' # Check all columns
+#' result_df3 <- filter_rows_not_all_equal(df)
+#' print(result_df3)
+filter_rows_not_all_equal <- function(input_tibble, columns_to_check = NULL, columns_to_exclude = NULL) {
+  if (!is.null(columns_to_check)) {
+    result_tibble <- input_tibble %>%
+      filter(
+        (!do.call(pmin, dplyr::select(., dplyr::all_of(columns_to_check))) ==
+           do.call(pmax, dplyr::select(., dplyr::all_of(columns_to_check))))
+        | is.na(do.call(pmin, dplyr::select(., dplyr::all_of(columns_to_check))))
+      )
+  } else {
+    result_tibble <- input_tibble %>%
+      filter(
+        (!do.call(pmin, dplyr::select(., -dplyr::all_of(columns_to_exclude))) ==
+           do.call(pmax, dplyr::select(., -dplyr::all_of(columns_to_exclude))))
+        | is.na(do.call(pmin, dplyr::select(., -dplyr::all_of(columns_to_exclude))))
+      )
+  }
+  return(result_tibble)
+}
+
+#' Find column name equal to vector, if ant
+#'
+#' @param input_tibble
+#' @param target_vector
+#'
+#' @return A column name, or character(0) if there is no match
+#' @export
+#'
+#' @examples
+#' # Sample data
+#' df <- tibble(
+#'   A = c(1, 2, 1),
+#'   B = c(4, 5, 6),
+#'   C = c(7, 8, 9)
+#' )
+#'
+#' # Target vector to compare
+#' target_vector <- c(1, 2, 1)
+#'
+#' # Find columns equal to the target vector
+#' matching_columns <- find_equal_column(df, target_vector)
+#'
+#' # Print the result
+#' print(matching_columns)
+find_equal_column <- function(input_tibble, target_vector) {
+  matching_columns <- colnames(input_tibble)[sapply(input_tibble, function(col) all(col == target_vector))]
+  return(matching_columns)
+}
+
