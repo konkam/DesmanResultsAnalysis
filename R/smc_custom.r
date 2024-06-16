f568a4e4632b11fe05517d62f657894dcb532047
DanielBonnery <daniel.bonnery@gmail.com>
2024-06-16 23:57:13 +0200

    a

--- R/smc_custom.r
+++ R/smc_custom.r
@@ -445,4 +445,149 @@ smc_sampler<-function(n_vsa,
     
   }
   list(sample=new,traces=if(trace_all){traces}else{NULL})
+}
+
+
+
+
+
+
+
+
+#'@description SMC algorithm
+#'@param i : integer >0, number of particles
+#'@param g : integer >0, number of variants in model
+#'@param n_vsa : an array of integers, index by v(position) s (sample) and a (nucleotide a,c,g,t)
+#'@examples
+#'n=1000;v=20;g=5;s=3;alpha_pi=.1
+#'sim=sim_tau_pi_epsilon_n(v=v, g=g, s=s, n=n, error_rate = .001, alpha_pi=alpha_pi)
+#'n_vsa=sim$n_vsa
+#'i=100
+#'init=list(pi_igs=plyr::raply(i,sim$pi_gs),
+#'epsilon_iba=plyr::raply(i,sim$epsilon_ba),
+#'tau_ivga=plyr::raply(i,sim$tau_vgb))
+#'n_vsa_df=reorder_counts(n_vsa = n_vsa,seed=seed)
+#'n_vsa_lambda=sub_sample_counts(n_vsa_df,lambda=.5)
+#'delta=1e-3
+#'ess_min=60
+#'max_lambda=1
+#'min_lambda=.5
+#'unaccounted_for=unaccounted_for_f(n_vsa_df,lambda=min_lambda)
+#' t_max=30
+#' t_min=30
+#' n_plus=sum(n_vsa)
+#' trace_all=TRUE
+#' .update_lambda=update_lambda
+#' init=NULL 
+#' shape_epsilon=c(1,1000)
+#' mcmc=TRUE
+#' new=smc_sampler(n_vsa=n_vsa,g=g,t_min=t_min,t_max=t_max,i=i,ess_min=ess_min,init=NULL)
+#' save(new,file="new.rda")
+#' new$traces$ess
+#' 
+smc_custom<-function(n_vsa,
+                      seed=1,
+                     n_vsa,
+                     gs="jags",
+                     tau_vgb=NULL,
+                     G=if(!is.null(tau_vgb)){dim(tau_vgb)[2]}else{5},
+                     block_tau=TRUE,
+                     alpha_tau=NULL,
+                     alpha_epsilon=NULL,
+                     bar_epsilon_1_std=NULL,
+                     bar_epsilon_1_mean=NULL,
+                     alpha_bar_epsilon=c(1,10),
+                     bar_epsilon_1=NULL,
+                     kappa_rho=NULL,
+                     alpha_pi=.1,
+                     n_chains = 2,
+                      n_vsa_df=reorder_counts(n_vsa = n_vsa,seed=seed),
+                      g=G,
+                      t_min=1,
+                      t_max,
+                      ess_min=NULL,
+                      smc_kernel=desman_kernel,
+                      trace_all=TRUE,
+                      .update_lambda=update_lambda,
+                      mcmc=FALSE,
+                     ...){
+  
+  lambda=0.1
+  rep_alpha_pi=rep(alpha_pi,g)
+  t=0
+  
+  n_plus=sum(n_vsa)
+  v=dim(n_vsa)[1]
+  s=dim(n_vsa)[2]
+  new=if(is.null(init)){{
+    epsilon_iba=rbeta(n_chains,shape_epsilon[1],shape_epsilon[2])|>plyr::aaply(1,epsilon_ba_f)
+    tau_ivga <- plyr::raply(n_chains,sim_tau_vgb(v = v, g = g))
+    pi_igs <- plyr::raply(n_chains,sim_pi_gs(g = g, s = s, alpha_pi = alpha_pi))
+    list(epsilon_iba=epsilon_iba,tau_ivga=tau_ivga,pi_igs=pi_igs)}}else{init}
+  
+  #w<-smc_b_prime(0*n_vsa,tau_ivga,pi_igs,epsilon_iba)
+  if(!mcmc){ww<-rep(1/n_chains,n_chains)}#w/sum(w)}
+  if(trace_all){
+    traces=list(epsilon_iba=array(new$epsilon_iba,c(1,dim(new$epsilon_iba))),
+                tau_ivga=array(new$tau_ivga,c(1,dim(new$tau_ivga))),
+                pi_igs=array(new$pi_igs,c(1,dim(new$pi_igs))),
+                ww=array(ww,c(1,dim(as.array(ww)))),
+                sample_i=array(1:n_chains,c(1,n_chains)),
+                ess=1)
+    
   }
+  if(mcmc){n_vsa_lambda=n_vsa;lambda=0;sample_i=1:n_chains}
+  while(((lambda<1)|(t<=min(t_max,t_min)))&(t<max(t_max,t_min))){
+    print(paste0("t : ",t," || lambda :",lambda))
+    t=t+1
+    if(!mcmc){
+      n_vsa_lambda=data_tempering_stratified(n_vsa=n_vsa,n_plus=n_plus,n_vsa_df=n_vsa_df,lambda=lambda)
+      sample_i=sample(x=n_chains,size=n_chains,replace=(n_chains>1),prob=ww)}
+    
+    new0=new
+    new=plyr::llply(new,resample_array,dimension=1,selection=sample_i)
+    new=with(new,
+             plyr::alply(seq_len(n_chains),1,
+                         function(i){
+                           smc_kernel(n_vsa_lambda,
+                                      tau_ivga[n_chains,,,],
+                                      pi_igs[n_chains,,],
+                                      epsilon_iba[n_chains,,],
+                                      rep_alpha_pi,
+                                      delta)}))|>
+      (function(x){plyr::alply(c("tau_vgb","pi_gs","epsilon_ba"),1,function(parameter){
+        plyr::laply(x,`[[`,parameter)})})()|>
+      setNames(c("tau_ivga","pi_igs","epsilon_iba"))
+    
+    if(!mcmc){
+      w<-do.call(what=smc_b_prime,c(list(n_vsa_lambda=n_vsa_lambda),new[c("tau_ivga","pi_igs","epsilon_iba")]))
+      w<-w/max(w)
+      ww<-w/sum(w)
+      ess=1/sum(ww^2)
+      sample_size=sum(n_vsa_lambda)
+      lambda_n_vsa=.update_lambda(ess_min,
+                                  n_vsa=n_vsa,
+                                  n_plus=n_plus,seed=seed,
+                                  n_vsa_df=n_vsa_df,
+                                  min_samplesize=sample_size,
+                                  max_samplesize=n_plus,
+                                  tau_ivga,
+                                  pi_igs,
+                                  epsilon_iba)
+      lambda=max(lambda_n_vsa$lambda,lambda*1.02)
+      n_vsa_lambda=lambda_n_vsa$n_vsa_lambda}
+    
+    
+    
+    if(trace_all){
+      traces=list(epsilon_iba=abind::abind(traces$epsilon_iba,new$epsilon_iba,along=1),
+                  pi_igs=abind::abind(traces$pi_igs,new$pi_igs,along=1),
+                  tau_ivga=abind::abind(traces$tau_ivga,new$tau_ivga,along=1),
+                  ww=abind::abind(traces$ww,array(ww,c(1,n_chains)),along=1),
+                  sample_i=abind::abind(traces$sample_i,array(sample_i,c(1,n_chains)),along=1),
+                  ess=c(traces$ess,1/(sum(ww^2))))}
+    
+    
+  }
+  list(sample=new,traces=if(trace_all){traces}else{NULL})
+}
